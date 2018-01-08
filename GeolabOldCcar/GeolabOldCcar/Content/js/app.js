e6b4ea444f68321124f50c61bb800d0ee555d70f
fidofido21 <fido.osiashvili@gmail.com>
2018-01-08 15:22:11 +0400

    front add

---
+++ GeolabOldCcar/GeolabOldCcar/Content/js/app.js
@@ -0,0 +1,28 @@
+$('.menu-toggle').click(function() {
+  
+  $('.site-nav').toggleClass('site-nav--open', 500);
+  $(this).toggleClass('open');
+
+  var src = ($(this).attr('src') === '/Content/images/auto_logo_red.png')
+      ? '/Content/images/auto_logo.png'
+      : '/Content/images/auto_logo_red.png';
+         $(this).attr('src', src)
+
+         $('#main_logo').attr('src',src);
+
+   $('.left_control').toggleClass('hide');  
+   $('.right_control').toggleClass('hide');      
+
+})
+
+function initMap() {
+        var uluru = {lat: 41.71751392, lng: 44.78465617};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
